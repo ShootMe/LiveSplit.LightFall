114f01b18f1dceb4cdd0ca4ca1f61dcd86ce0f0b
ShootMe <birreller@gmail.com>
2018-07-02 14:11:49 -0600

    New patch

--- SplitterMemory.cs
+++ SplitterMemory.cs
@@ -3,7 +3,7 @@ using System;
 using System.Diagnostics;
 namespace LiveSplit.LightFall {
 	public partial class SplitterMemory {
-		private static ProgramPointer Manager = new ProgramPointer(AutoDeref.Single, new ProgramSignature(PointerVersion.Steam, "558BEC83EC088B05????????83EC086A0050E8????????83C41085C00F84????????8B05", 8));
+		private static ProgramPointer Singletons = new ProgramPointer(AutoDeref.Single, new ProgramSignature(PointerVersion.Steam, "558BEC83EC088B05????????83EC086A0050E8????????83C41085C00F84????????8B05", 8));
 		public Process Program { get; set; }
 		public bool IsHooked { get; set; } = false;
 		public DateTime LastHooked;
@@ -12,26 +12,31 @@ namespace LiveSplit.LightFall {
 			LastHooked = DateTime.MinValue;
 		}
 		public string RAMPointers() {
-			return Manager.GetPointer(Program).ToString("X");
+			return Singletons.GetPointer(Program).ToString("X");
 		}
 		public string RAMPointerVersion() {
-			return Manager.Version.ToString();
+			return Singletons.Version.ToString();
 		}
 		public Level CurrentLevel() {
-			return (Level)Manager.Read<int>(Program, -0x38, 0x30);
+			//Singletons.levelManager.currentLevel
+			return (Level)Singletons.Read<int>(Program, -0x3c, 0x30);
 		}
 		public Level NextLevel() {
-			return (Level)Manager.Read<int>(Program, -0x38, 0x28);
+			//Singletons.levelManager.nextLevel
+			return (Level)Singletons.Read<int>(Program, -0x3c, 0x28);
 		}
 		public Level LevelLoading() {
-			return (Level)Manager.Read<int>(Program, -0x38, 0x2c);
+			//Singletons.levelManager.levelLoading
+			return (Level)Singletons.Read<int>(Program, -0x3c, 0x2c);
 		}
 		public bool ReachedEnd() {
-			return Manager.Read<bool>(Program, -0x38, 0x34);
+			//Singletons.levelManager.hasReachedEnd
+			return Singletons.Read<bool>(Program, -0x3c, 0x34);
 		}
 		public float GameTime() {
+			//Singletons.gameMode.runTime
 			IntFloat time = default(IntFloat);
-			time.FloatVal = Manager.Read<float>(Program, -0x24, 0x18);
+			time.FloatVal = Singletons.Read<float>(Program, -0x28, 0x18);
 			time.IntVal ^= 230887;
 			return time.FloatVal;
 		}
